710333b33050dcd2c0d91a6d9020bd07e556246e
Kelvin Wallace <85933290+litmas@users.noreply.github.com>
2025-05-09 16:41:00 +0200

    Update auth.js

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,7 +1,13 @@
 const { verifyToken } = require('../config/jwt');
 const AppError = require('../utils/appError');
-const User = require('../models/userModel'); 
+const User = require('../models/userModel');
 
+/**
+ * Protects a resource by checking if the user is authenticated
+ *
+ * @param {Function} middlewareFunction - The middleware function to check if the user is authenticated
+ * @returns {Function} - A middleware function to protect the resource
+ */
 exports.protect = async (req, res, next) => {
   try {
     let token;
@@ -12,9 +18,13 @@ exports.protect = async (req, res, next) => {
     if (!token) {
       return next(new AppError('You are not logged in! Please log in to get access.', 401));
     }
-    
+
     const decoded = verifyToken(token);
 
+    /**
+     * Retrieves the current user by decoding the provided ID.
+     * @type {Object}
+     */
     const currentUser = await User.findById(decoded.id);
     if (!currentUser) {
       return next(new AppError('The user belonging to this token does no longer exist.', 401));
