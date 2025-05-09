ce9644d6b2d5991f7a2d2c332e2807399757c2d8
Kelvin Wallace <85933290+litmas@users.noreply.github.com>
2025-05-09 16:41:34 +0200

    Update appError.js

--- src/utils/appError.js
+++ src/utils/appError.js
@@ -1,13 +1,20 @@
 class AppError extends Error {
-    constructor(message, statusCode) {
-      super(message);
-  
-      this.statusCode = statusCode;
-      this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
-      this.isOperational = true;
-  
-      Error.captureStackTrace(this, this.constructor);
-    }
+  /**
+   * Constructor for creating a custom error object.
+   *
+   * @param {string} message - The error message.
+   * @param {number} statusCode - The HTTP status code of the error.
+   *
+   * @return {void}
+   */
+  constructor(message, statusCode, details = null) {
+    super(message);
+    this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+    this.details = details;
+    this.isOperational = true;
+    Error.captureStackTrace(this, this.constructor);
   }
-  
-  module.exports = AppError;
+}
+
+module.exports = AppError;
